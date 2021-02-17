ff8a9c5142c8b933ecc1b1b793d0c56a761059cb
Teitei011 <stefantleal14@gmail.com>
2021-02-17 17:41:51 -0300

    Teste

--- covid/upload2TheCloud.sh
+++ covid/upload2TheCloud.sh
@@ -1,13 +1,3 @@
-echo "Processing Data..."
-
-cd /mnt/c/Users/stefa/Documents/GitHub/WorkingWithData/
-python3 SplitDatasheet.py 
-cd /mnt/c/Users/stefa/Documents/GitHub/Site/Teitei011.github.io/covid
-
-echo "Copying the files to my folder..."
-
-cp -r /mnt/c/Users/stefa/Documents/GitHub/WorkingWithData/brazil /mnt/c/Users/stefa/Documents/GitHub/Site/Teitei011.github.io/covid
-
 git add .
 
 echo "Adding csv into git..."
