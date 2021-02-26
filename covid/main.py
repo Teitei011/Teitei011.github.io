de1c5260495ddf9f2e6971ffbc05844cf36adda8
Teitei011 <stefantleal14@gmail.com>
2021-02-26 15:10:54 -0300

    Main function: TO automate for me

---
+++ covid/main.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import time
+import webbrowser
+import pyautogui as py
+import os
+import schedule
+
+
+PATH = "brazil/"
+FILE_NAME = "Brazil.csv"
+
+X_POSITION, Y_POSITION = 0 , 0
+
+#@jit(nopython=True, parallel=True)
+def extractDataframeByName(dataframe, location):
+    print("Processing... 0%")
+    all_names = dataframe[location].unique().tolist()
+
+    number_of_names = len(all_names)
+    counter = 0
+
+    for name in all_names:
+        counter +=1 
+
+        #print("Processing... {}  - {:.2f}%".format(name, (counter*100)/number_of_names))
+        if (counter % 530 == 0):
+            print("Processing... {:.0f}%".format( (counter*100)/number_of_names))
+
+
+        newDataframe = dataframe.loc[dataframe[location] == name]
+        newDataframe.drop(newDataframe.index[0])
+        newDataframe.to_csv(f"{PATH}/{name}.csv", index=False)
+
+
+def divideByTwoWhenPossible(array):
+    for i in range(len(array)):
+        try:
+            array[i] = array[i] / 2
+        except:
+            pass
+    return array
+
+
+def changeDateOrder(array):
+    newArray = []
+    for date in array:
+        buffer = date.split("-")
+        newArray.append(buffer[2] + "-" + buffer[1] + "-" + buffer[0])
+    return newArray
+
+def splitDataframe2Something(dataframe, name): # Regiao pro Brasil né? # Estado tem que dividr por 2
+
+    if (name == "estado"):
+        newDataframe = dataframe.groupby([name, "data"])[['casosAcumulado', 'casosNovos', 'obitosAcumulado', 'obitosNovos']].sum().apply(lambda x: divideByTwoWhenPossible(x))
+    elif (name == "Brasil"):
+        dataframe = dataframe.loc[dataframe["regiao"] == "Brasil"]
+        newDataframe = dataframe[["data", "casosAcumulado", "casosNovos", "obitosAcumulado", "obitosNovos"]].copy()
+ 
+    else: # Municipios
+         newDataframe = dataframe.groupby([name, "data"])[['casosAcumulado', 'casosNovos', 'obitosAcumulado', 'obitosNovos']].sum()
+    
+    newDataframe.reset_index(inplace = True)
+    newDataframe["data"] = changeDateOrder(newDataframe["data"])
+    newDataframe["daily cases moving average"] = newDataframe['casosNovos'].rolling(window=14).mean()
+    newDataframe["daily deaths moving average"] = newDataframe['obitosNovos'].rolling(window=14).mean()
+    newDataframe["sum_of_daily_cases_week"] = newDataframe['casosNovos'].rolling(window=7).sum() 
+    newDataframe["sum_of_daily_deaths_week"] = newDataframe['obitosNovos'].rolling(window=7).sum() 
+
+    
+    return newDataframe
+
+def downloadData():
+    webbrowser.open("https://covid.saude.gov.br/")
+
+
+    seeScreenSize()
+    # Click in the right button
+    time.sleep(15) 
+    py.click(X_POSITION, Y_POSITION, duration=0.25)
+    time.sleep(60)
+
+    # CHange the file's name
+
+    os.system("cp ~/Downloads/*.csv Brazil.csv")
+
+
+def processData():
+    tempo_inicial = time.time()
+
+    #def pre_processing_csv_data(file_name):
+    unprocessedDataset = pd.read_csv(str(FILE_NAME), delimiter=";", error_bad_lines=False)
+
+    cities = splitDataframe2Something(unprocessedDataset, "municipio")
+    states = splitDataframe2Something(unprocessedDataset, "estado")
+    brazil = splitDataframe2Something(unprocessedDataset, "Brasil")
+    brazil.to_csv(f"{PATH}/Brasil.csv", index=False)
+
+    extractDataframeByName(cities, "municipio")
+    extractDataframeByName(states, "estado")
+
+    print("It took {:.2f} minutes".format((time.time() - tempo_inicial)/60) )
+
+    os.system("rm Brazil.csv")
+    os.system("./upload2TheCloud.sh")
+
+
+
+def tellMeThePosition(intervalBetweenTheResponses):
+    time.sleep(intervalBetweenTheResponses)
+    for i in range(10):
+        time.sleep(intervalBetweenTheResponses)
+        print(py.position())
+
+
+def seeScreenSize():
+    wh = py.size() 
+    if (wh[0] > 1400):
+        X_POSITION, Y_POSITION = 2550, 259
+    else:
+        X_POSITION, Y_POSITION = 1183, 267
+
+def main():
+    downloadData()
+    processData()
+
+
+schedule.every().day.at("15:10").do(main)
+
+
+
+# if __name__ == "__main__":
+#     main()
+
+    
