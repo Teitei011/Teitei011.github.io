bd2dc70c4bb52cf4cfb2cd41856dc6e63e5410a1
Teitei011 <stefantleal14@gmail.com>
2021-03-02 21:27:32 -0300

    CSV updated

--- covid/index.js
+++ covid/index.js
@@ -104,7 +104,7 @@ function setupBoxWithData(covidCases, locationName){
   //document.getElementById("DadosAtualizados").innerHTML = "<center>Esperando o Ministério da Saúde atualizar seus dados: " +"</center>" ;
  
  
-  document.getElementById("DadosAtualizados").innerHTML = "<center>Dados atualizados no dia: "  + covidCases.date[covidCases.cases.length -1] + " 20:00" +"</center>" ;
+  document.getElementById("DadosAtualizados").innerHTML = "<center>Dados atualizados no dia: "  + covidCases.date[covidCases.cases.length -1] + " 21:00" +"</center>" ;
   // document.getElementById("DadosAtualizados").innerHTML = "<center>Dados atualizados no dia: "  + "24/02/2021" + " 09:53" +"</center>" ;
 }
 
