b1d5b4732c08cafc975b20a9a8bfbf96ecd0b009
Teitei011 <stefantleal14@gmail.com>
2021-05-17 10:47:11 -0300

    CSV updated

--- covid/main.py
+++ covid/main.py
@@ -30,20 +30,21 @@ def extractDataframeByName(dataframe, location):
     counter = 0
 
     for name in all_names:
-        counter +=1 
+        counter += 1
 
         #print("Processing... {}  - {:.2f}%".format(name, (counter*100)/number_of_names))
         if (counter % 530 == 0):
-            print("Processing... {:.0f}% ".format( (counter*100)/number_of_names))
-
+            print("Processing... {:.0f}% ".format(
+                (counter*100)/number_of_names))
 
         newDataframe = dataframe.loc[dataframe[location] == name]
         newDataframe.drop(newDataframe.index[0])
-        newDataframe.reset_index(inplace = True)
+        newDataframe.reset_index(inplace=True)
 
         newDataframe.to_csv(f"{PATH}/{name}.csv", index=False)
 
         #newDataframe.to_json(f"{PATH}/{name}.json", orient ='values')
+
 
 def divideByTwoWhenPossible(array):
     for i in range(len(array)):
@@ -65,24 +66,35 @@ def changeDateOrder(array):
 # In[35]:
 
 
-def splitDataframe2Something(dataframe, name): # Regiao pro Brasil né? # Estado tem que dividr por 2
-    dataframe.data = [str(dataframe.data[i]).replace("/", "-") for i in range(len(dataframe))]
+# Regiao pro Brasil né? # Estado tem que dividr por 2
+def splitDataframe2Something(dataframe, name):
+    dataframe.data = [str(dataframe.data[i]).replace("/", "-")
+                      for i in range(len(dataframe))]
     if (name == "estado"):
-        newDataframe = dataframe.groupby([name, "data"])[['casosAcumulado', 'casosNovos', 'obitosAcumulado', 'obitosNovos']].sum().apply(lambda x: divideByTwoWhenPossible(x))
+        newDataframe = dataframe.groupby([name, "data"])[
+            ['casosAcumulado', 'casosNovos', 'obitosAcumulado', 'obitosNovos']].sum().apply(lambda x: divideByTwoWhenPossible(x))
+        newDataframe[name] = newDataframe[name].replace(['AC', 'AL', "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"], ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+                                                                "Espírito Santo", "Goiás", "Maranhão",  "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"])
+
     elif (name == "Brasil"):
         dataframe = dataframe.loc[dataframe["regiao"] == "Brasil"]
-        newDataframe = dataframe[["data", "casosAcumulado", "casosNovos", "obitosAcumulado", "obitosNovos"]].copy()
- 
-    else: # Municipios
-         newDataframe = dataframe.groupby([name, "data"])[['casosAcumulado', 'casosNovos', 'obitosAcumulado', 'obitosNovos']].sum()
-    
-    newDataframe.reset_index(inplace = True)
-    newDataframe["daily_cases_moving_average"] = newDataframe['casosNovos'].rolling(window=7).mean()
-    newDataframe["daily_deaths_moving_average"] = newDataframe['obitosNovos'].rolling(window=7).mean()
-    newDataframe["sum_of_daily_cases_week"] = newDataframe['casosNovos'].rolling(window=7).sum() 
-    newDataframe["sum_of_daily_deaths_week"] = newDataframe['obitosNovos'].rolling(window=7).sum() 
+        newDataframe = dataframe[["data", "casosAcumulado",
+                                  "casosNovos", "obitosAcumulado", "obitosNovos"]].copy()
 
-    
+    else:  # Municipios
+        newDataframe = dataframe.groupby([name, "data"])[
+            ['casosAcumulado', 'casosNovos', 'obitosAcumulado', 'obitosNovos']].sum()
+
+    newDataframe.reset_index(inplace=True)
+    newDataframe["daily_cases_moving_average"] = newDataframe['casosNovos'].rolling(
+        window=7).mean()
+    newDataframe["daily_deaths_moving_average"] = newDataframe['obitosNovos'].rolling(
+        window=7).mean()
+    newDataframe["sum_of_daily_cases_week"] = newDataframe['casosNovos'].rolling(
+        window=7).sum()
+    newDataframe["sum_of_daily_deaths_week"] = newDataframe['obitosNovos'].rolling(
+        window=7).sum()
+
     return newDataframe
 
 
@@ -91,26 +103,31 @@ def splitDataframe2Something(dataframe, name): # Regiao pro Brasil né? # Estado
 
 def main():
 
-    #def pre_processing_csv_data(file_name):
-    unprocessedDataset = pd.read_csv(str(FILE_NAME), delimiter=";", error_bad_lines=False)
+    # def pre_processing_csv_data(file_name):
+    unprocessedDataset = pd.read_csv(
+        str(FILE_NAME), delimiter=";", error_bad_lines=False)
 
     cities = splitDataframe2Something(unprocessedDataset, "municipio")
     states = splitDataframe2Something(unprocessedDataset, "estado")
+     states["estado"] = states["estado"].replace(['AC', 'AL', "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"], ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+                                                                "Espírito Santo", "Goiás", "Maranhão",  "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"])
+
 #     print(states)
-    
+
     brazil = splitDataframe2Something(unprocessedDataset, "Brasil")
     brazil.to_csv(f"{PATH}/Brasil.csv", index=False)
-    
+
     #brazil["data"] = changeDateOrder(brazil["data"])
     #brazil.to_json(f"{PATH}/Brasil.json", orient ='values')
-    
+
     extractDataframeByName(cities, "municipio")
     extractDataframeByName(states, "estado")
 
-    print("It took {:.2f} minutes".format((time.time() - tempo_inicial)/60) )
+    print("It took {:.2f} minutes".format((time.time() - tempo_inicial)/60))
 
     os.system("rm Brazil.csv")
     os.system("./upload2TheCloud.sh")
+
 
 if __name__ == "__main__":
     main()
@@ -119,11 +136,4 @@ if __name__ == "__main__":
 # In[ ]:
 
 
-
-
-
 # In[ ]:
-
-
-
-
