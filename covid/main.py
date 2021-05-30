6dde3e4e3bae80037c45ab8f953a1be42364e05b
Teitei011 <stefantleal14@gmail.com>
2021-05-29 22:38:52 -0300

    CSV updated

--- covid/main.py
+++ covid/main.py
@@ -107,15 +107,12 @@ def main():
         str(FILE_NAME), delimiter=";", error_bad_lines=False)
 
     cities = splitDataframe2Something(unprocessedDataset, "municipio")
-    print("Cities")
     states = splitDataframe2Something(unprocessedDataset, "estado")
     states["estado"] = states["estado"].replace(['AC', 'AL', "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"], ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",  "Espírito Santo", "Goiás", "Maranhão",  "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"])
 
-    print("States")
 
     brazil = splitDataframe2Something(unprocessedDataset, "Brasil")
 
-    print("Brasil")
     brazil.to_csv(f"{PATH}/Brasil.csv", index=False)
 
     #brazil["data"] = changeDateOrder(brazil["data"])
